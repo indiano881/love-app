f1278b6f2762f73e10267adf51fb1978699045ce
Davide Baldi <d.baldi88@gmail.com>
2024-08-29 18:57:02 +0200

    save 2 test

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,8 +1,16 @@
 import {render, screen} from "@testing-library/react";
 import Header from ".";
+describe("PAge header", ()=> {
+    test("Renders page title", ()=> {
+        render(<Header />)
+        const headerText=screen.getByText(/love app/i);
+        expect(headerText).toBeInTheDocument();
+    })
+    test("Renders page SUBtitle", ()=> {
+        render(<Header />)
+        const headerText=screen.getByRole("heading", {level: 2});
+        expect(headerText).toBeInTheDocument();
+    })
 
-test("Renders page title", ()=> {
-    render(<Header />)
-    const headerText=screen.getByText(/love app/i);
-    expect(headerText).toBeInTheDocument();
+
 })
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ const Header = () => {
     return (
         <div>
           <h1>Love APP</h1>
+          <h2>Find your perfect date!</h2>
         </div>
     )
 };
